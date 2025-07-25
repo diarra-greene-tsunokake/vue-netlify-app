444e6f7e97ce32507ed55a063b3087de2c0b1c48
D Greene <deegreenemusic@gmail.com>
2025-07-26 02:39:31 +0900

    Update HelloWorld.vue

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
 </script>
 
 <template>
-   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/contact">Contact</router-link>
+   <nav class="nav-container">
+    <router-link to="/" class="home-nav">Home</router-link> 
+    <router-link to="/contact" class="home-nav">Contact</router-link>
   </nav>
   <router-view />
 </template>
@@ -22,5 +22,21 @@
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+.nav-container {
+  display: flex;
+  flex-direction: row; /* Default: row, but good to be explicit */
+  gap: 20px; /* Optional spacing between links */
+  padding-top: 20px;
+  padding-left: 20px;
+  background-color: antiquewhite;
+padding-bottom: 10px;
+}
+
+.home-nav {
+  font-weight: bold;
+  font-size: 1.8em;
+  color: black;
+  text-decoration: none;
 }
 </style>
